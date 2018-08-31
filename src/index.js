036e547ff2ffb28cdf04b280c59a417ebf869564
Haroon Ahmed <haroonrmit@gmail.com>
2018-08-31 05:37:46 +0400

    commiting index.js file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './components/WeatherApp';
+import WeatherApp from './components/WeatherApp';
+
+ReactDOM.render(<WeatherApp />, document.getElementById('root'));
